9b0bffd362cecf9b1393c5df29c802e9d91f1594
tolbastue <67708771+tolbastue@users.noreply.github.com>
2021-06-20 13:13:56 +0200

    deleted not required classes

--- projects/Hunter and Prey Game/src/com/learningjava/Bildschirm.java
+++
@@ -1,9 +0,0 @@
-package com.learningjava;
-
-public class Bildschirm {
-    // Schreibt 100 Leerzeichen auf den Bildschirm
-    public static void loeschen() {
-        for(int i = 0; i < 100; i++)
-            System.out.println();
-    }
-}
